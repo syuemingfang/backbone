6f6e852f6963f868a9e1cee5a0749d8e661b1942
syuemingfang <syuemingfang@yahoo.com.tw>
2013-07-22 18:30:24 +0800

    update 13072202

--- mysql.php
+++ mysql.php
@@ -7,27 +7,36 @@ if(isset($_GET['act'])){
 	$conID=mysql_pconnect($mysqlHost, $mysqlUser, $mysqlPass)or die('Error: '.mysql_error().'<br />');
 	@mysql_select_db($mysqlDB, $conID);
 	if($_GET['act'] == 'createDatabase'){
-		@mysql_query('Set names=utf8')or die('Error: '.mysql_error().'<br />');
+
+		@mysql_query('Set names=utf8');
 		@mysql_query('Set character_set_client=utf8')or die('Error: '.mysql_error().'<br />');
 		@mysql_query('Set character_set_results=utf8')or die('Error: '.mysql_error().'<br />');
 		@mysql_query('Set character_set_connection=utf8')or die('Error: '.mysql_error().'<br />');
 		@mysql_query('Set collation_connection=utf8_general_ci')or die('Error: '.mysql_error().'<br />');
 		echo '<h2>Database</h2>';
-		@mysql_query('Create  Database '.$_POST['mysqlDB'], $conID)or die('Error: '.mysql_error().'<br />');
-		echo 'Okay: '.$_POST['mysqlDB'].'<br />';
+		@mysql_query('Drop Database '.$mysqlDB, $conID);
+		@mysql_query('Create Database '.$mysqlDB, $conID);
+		@mysql_select_db($mysqlDB, $conID);
+		echo 'Okay: '.$mysqlDB.'<br />';
 		echo '<h2>Table</h2>';
 		$query='Create Table student(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), nickname VARCHAR(255), age INT(11))';
-		$result=@mysql_query($query, $conID)or die('Error: '.$query.'<br />');
+		$result=@mysql_query($query, $conID);
 		echo 'Okay: student<br />';
 		echo '<h2>Write</h2>';
 		$data=array(
 			array('id'=>'1', 'nickname'=>'Tom', 'age'=>'20'),
 			array('id'=>'2', 'nickname'=>'Jack', 'age'=>'18'),
-			array('id'=>'3', 'nickname'=>'Sam', 'age'=>'25')
+			array('id'=>'3', 'nickname'=>'Sam', 'age'=>'25'),
+			array('id'=>'4', 'nickname'=>'John', 'age'=>'22'),
+			array('id'=>'5', 'nickname'=>'Jack', 'age'=>'17'),
+			array('id'=>'6', 'nickname'=>'Frank', 'age'=>'25'),
+			array('id'=>'7', 'nickname'=>'Peter', 'age'=>'25'),
+			array('id'=>'8', 'nickname'=>'Lee', 'age'=>'15'),
+			array('id'=>'9', 'nickname'=>'Ray', 'age'=>'10')						
 		);
 		for($i=0; $i < count($data); $i++){
 			$query='Insert Into student(id, nickname, age) Values ('.$data[$i]['id'].', "'.$data[$i]['nickname'].'", '.$data[$i]['age'].');';
-			$result=@mysql_query($query, $conID)or die('Error: '.$query.'<br />');
+			$result=@mysql_query($query, $conID);
 			echo 'Okay: '.$query.'<br />';
 		}
 		echo 'MySQL quite ready now.<br />';
@@ -54,7 +63,7 @@ if(isset($_GET['act'])){
 		$result=@mysql_query($query, $conID)or die('Error: '.$query.'<br />');
 		$rows=@mysql_fetch_row($result);
 		$newId=$rows[0]+1;
-		$query='Insert Into student(id, nickname) Values ('.$newId.', "'.$data->{'nickname'}.'");';
+		$query='Insert Into student(id, nickname, age) Values ('.$newId.', "'.$data->{'nickname'}.'", "'.$data->{'age'}.'");';
 		$result=@mysql_query($query, $conID)or die('Error: '.$query.'<br />');
 	} else if($_GET['act'] == 'update'){
 		$data=json_decode(file_get_contents('php://input'));
